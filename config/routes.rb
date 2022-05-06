effc64c753bab0daa7d446bca7a22eda8c8f4d5a
shinya-kaneshiro <armageddn1@gmail.com>
2022-05-06 21:12:48 +0900

    Temporary login screen creation completed

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,13 @@
 Rails.application.routes.draw do
+
+  # top-page
   root 'static_pages#top'
+
+  # login/logout
+  get    '/login', to: 'sessions#new'
+  post   '/login', to: 'sessions#create'
+  delete '/logout', to: 'sessions#destroy'
   
+  # Messaging-API
   post '/callback', to: 'webhooks#callback'
 end
