d198ebd61485e5baa5c70afa6641e4ede90f43a0
shinya-kaneshiro <armageddn1@gmail.com>
2022-05-06 12:01:35 +0900

    Check point 20220506_1200

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,13 @@
 module ApplicationHelper
+
+    def full_title(page_name = "")
+        # base_title = "Sample"
+        base_title = "AgainstFc"
+        if page_name.empty?
+          base_title
+        else
+          page_name + " | " + base_title
+        end
+      end
+    
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,5 @@
 Rails.application.routes.draw do
+  root 'static_pages#top'
+  
   post '/callback', to: 'webhooks#callback'
 end
