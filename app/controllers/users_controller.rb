a67ea8eeb9581ede91d7196a37cb575afd96fed3
shinya-kaneshiro <armageddn1@gmail.com>
2022-05-12 02:44:58 +0900

    Implementation of new user registration function

---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,27 @@
+class UsersController < ApplicationController
+
+  def new
+    @user = User.new
+  end
+
+  def create
+    @user = User.new(user_params)
+    if @user.save
+      log_in(@user)
+      flash[:success] = "新規登録に成功しました。"
+      redirect_to root_path
+      # redirect_to user_path user
+    else
+      render :new
+    end
+  end
+
+
+
+
+  private
+    def user_params
+      params.require(:user).permit(:name, :email, :password, :password_confirmation)
+    end
+
+end
