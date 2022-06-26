6045de031e9f86610a3a35c33d831bc3aa702a74
shinya-kaneshiro <armageddn1@gmail.com>
2022-06-26 14:27:12 +0900

    Dynamically change the height of the page

--- app/javascript/main.js
+++ app/javascript/main.js
@@ -1,5 +1,37 @@
-document.addEventListener('turbolinks:load', () => {
-  // alert("test")
-  // $('[data-toggle="tooltip"]').tooltip()
-  // $('[data-toggle="popover"]').popover()
-})
+document.addEventListener('turbolinks:load', function () {
+const line = document.querySelector('.height-change-line')
+const el = document.querySelector('.jumbotron-long')
+const cb = function(entries, observer) {
+    entries.forEach(entry => {
+        if(entry.isIntersecting) {
+            console.log('inview')
+            el.classList.toggle('height-change')
+        } else {
+            console.log('outview')
+            el.classList.toggle('height-change')
+        }
+    })
+}
+
+const io = new IntersectionObserver(cb);
+io.observe(line);
+});
+
+// document.addEventListener('DOMContentLoaded', function () {
+// const line = document.querySelector('.height-change-line')
+// const el = document.querySelector('.jumbotron-long')
+// const cb = function(entries, observer) {
+//     entries.forEach(entry => {
+//         if(entry.isIntersecting) {
+//             console.log('inview')
+//             el.classList.toggle('height-change')
+//         } else {
+//             console.log('outview')
+//             el.classList.toggle('height-change')
+//         }
+//     })
+// }
+
+// const io = new IntersectionObserver(cb);
+// io.observe(line);
+// });
